6222d7d86c0c960d19728a5b85287c86375ac1f8
Philco Michel <phil9231@gmail.com>
2016-07-09 20:27:09 -0400

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,127 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'Apple juice', description:'Beverages', price: 2000)
+Product.create(name: 'Beer', description:'Beverages', price: 5000)
+Product.create(name: 'coffee', description:'Beverages', price:2000 )
+Product.create(name: 'Milk', description:'Beverages', price:2050)
+Product.create(name: 'orange', description:'Beverages', price:2000)
+Product.create(name: 'Soda', description:'Beverages', price: 1500)
+Product.create(name: 'coke', description:'Beverages', price:1500 )
+Product.create(name: 'Flour', description:'Canned&Packaged Foods', price:3500)
+Product.create(name: 'Green Beans,canned', description:'Canned&Packaged Foods', price:3500)
+Product.create(name: 'Honey', description:'Canned&Packaged Foods', price:2500)
+Product.create(name: 'Ketchup', description:'Canned&Packaged Foods', price:3750)
+Product.create(name: 'Mayonnaise', description:'Canned&Packaged Foods', price:2500)
+Product.create(name: 'Mushrooms', description:'Canned&Packaged Foods', price:1200)
+Product.create(name: 'Flour', description:'Canned&Packaged Foods', price:3500)
+Product.create(name: 'Mustard', description:'Canned&Packaged Foods', price:3099)
+Product.create(name: 'Nutella', description:'Canned&Packaged Foods', price:1000)
+Product.create(name: 'Oil', description:'Canned&Packaged Foods', price:1200)
+Product.create(name: 'Ellbow Maccaroni', description:'Canned&Packaged Foods', price:2900)
+Product.create(name: 'Spaghetti', description:'Canned&Packaged Foods', price:3500)
+Product.create(name: 'Peanut Butter', description:'Canned&Packaged Foods', price:3200)
+Product.create(name: 'Rice', description:'Canned&Packaged Foods', price:3000)
+Product.create(name: 'Bagles ', description:'Bakery, Breakfast, Cereal ', price:200)
+Product.create(name: 'Breads ', description:'Bakery, Breakfast, Cereal ', price:4500)
+Product.create(name: 'Corn Flakes ', description:'Bakery, Breakfast, Cereal ', price:2500)
+Product.create(name: 'Croissant ', description:'Bakery, Breakfast, Cereal ', price:3500)
+Product.create(name: 'Hot Dog', description:'Bakery, Breakfast, Cereal ', price:3200)
+Product.create(name: 'Oatmeal', description:'Bakery, Breakfast, Cereal ', price:3500)
+Product.create(name: ' Breakfast Sausage', description:'Frozen Foods ', price:4000)
+Product.create(name: 'Chicken Breasts', description:'Frozen Foods ', price:12000)
+Product.create(name: 'Chicken Nugget', description:'Frozen Foods ', price:7099)
+Product.create(name: 'Chicken Tenderlion', description:'Frozen Foods ', price:12000)
+Product.create(name: 'Flounder', description:'Frozen Foods ', price:15000)
+Product.create(name: 'Tilapia', description:'Frozen Foods ', price:10000)
+Product.create(name: 'French Fries', description:'Frozen Foods ', price:8000)
+Product.create(name: 'Fruits', description:'Frozen Foods ', price:7000)
+Product.create(name: ' Ground Beef', description:'Frozen Foods ', price:12000)
+Product.create(name: ' Ice Cream', description:'Frozen Foods ', price:6000)
+Product.create(name: ' Mixed Veggies', description:'Frozen Foods ', price:5000)
+Product.create(name: 'Waffles ', description:'Frozen Foods ', price:3500)
+Product.create(name: ' Apple', description:'Produce ', price:3500)
+Product.create(name: ' Carrots', description:'Produce ', price:3000)
+Product.create(name: ' Grapes', description:'Produce ', price:2500)
+Product.create(name: ' Lemons', description:'Produce ', price:3500)
+Product.create(name: 'Lettuce', description:'Produce ', price:3800)
+Product.create(name: ' Omions', description:'Produce ', price:2900)
+Product.create(name: ' Mushrooms', description:'Produce ', price:3900)
+Product.create(name: ' orange', description:'Produce ', price:6000)
+Product.create(name: ' Pears', description:'Produce ', price:1300)
+Product.create(name: ' Potatos', description:'Produce ', price:2000)
+Product.create(name: ' Tomatoes ', description:'Produce ', price:4000)
+Product.create(name: 'Bacon', description:'Refrigerated Foods', price:3900)
+Product.create(name: 'Beef stew Meat', description:'Refrigerated Foods', price:1000)
+Product.create(name: ' Butter', description:'Refrigerated Foods', price:8000)
+Product.create(name: 'Cheese', description:'Refrigerated Foods', price:3300)
+Product.create(name: 'Milk', description:'Refrigerated Foods', price:3500)
+Product.create(name: 'Chocolate Chip cookie', description:'Refrigerated Foods', price:3200)
+Product.create(name: 'Cold Cuts ham&Chicken', description:'Refrigerated Foods', price:5500)
+Product.create(name: 'Cottage Cheese', description:'Refrigerated Foods', price:3000)
+Product.create(name: ' Cream Cheese', description:'Refrigerated Foods', price:3000)
+Product.create(name: 'Cream', description:'Refrigerated Foods', price:3000)
+Product.create(name: 'Eggs', description:'Refrigerated Foods', price:1750)
+Product.create(name: 'Ground Beef', description:'Refrigerated Foods', price:11000)
+Product.create(name: 'Ground Turkey', description:'Refrigerated Foods', price:10000)
+Product.create(name: 'Ham', description:'Refrigerated Foods', price:11000)
+Product.create(name: 'Ribs', description:'Refrigerated Foods', price:12000)
+Product.create(name: 'Salami', description:'Refrigerated Foods', price:2750)
+Product.create(name: 'Sour Cream', description:'Refrigerated Foods', price:300)
+Product.create(name: 'Yogurt', description:'Refrigerated Foods', price:300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
