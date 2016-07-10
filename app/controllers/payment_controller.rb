295081f0f05f3f12d0eef6f3fe6e46f8106c513c
Philco Michel <phil9231@gmail.com>
2016-07-10 14:26:31 -0400

    payment form_active

---
+++ app/controllers/payment_controller.rb
@@ -0,0 +1,26 @@
+class PaymentController < ApplicationController
+	 def new
+    @amount = 456789
+  end
+
+  def create
+  # Amount in cents
+  @amount = 500
+
+  customer = Stripe::Customer.create(
+    :email => params[:stripeEmail],
+    :source  => params[:stripeToken]
+    )
+
+  payment = Stripe::Charge.create(
+    :customer    => customer.id,
+    :amount      => @amount,
+    :description => 'Rails Stripe customer',
+    :currency    => 'usd'
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_payment_path
+  end
+end
